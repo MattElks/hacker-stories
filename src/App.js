91402a16294a6e4d9ba394b1775c6d4a6ed77c6e
matt_elks <mattelksdeveloper@gmail.com>
2022-08-23 09:55:36 +0100

    hacker-stories reactjs app initialized

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+
+const title = "Hello new world";
+
+const welcome = {
+  name: "Matt",
+  message: "Howdy, ",
+};
+
+const numArr = [1, 2, 3, 4, 5];
+const arrList = numArr.map((num) => <li>{num}</li>);
+
+function getTitle(title) {
+  return title;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{getTitle(title)}</h1>
+      <p>{welcome.message + welcome.name}</p>
+      <ul>{arrList}</ul>
+      <label htmlFor="search">Search: </label>
+      <input id="input" type="text" />
     </div>
   );
 }
